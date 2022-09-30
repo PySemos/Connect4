f7d68cb8a94cc92735d22b3110b4ac66fbc8733f
Celso <95507758+PySemos@users.noreply.github.com>
2022-09-30 08:28:50 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -8,7 +8,12 @@ public class Board{
    private int size = 8;
    private String[][] board;
    private String nonPlayedPosition = "-";
-   Board(){
+   private Boolean terminated = false;
+   private Player[] players;
+   Board(Player player1, Player player2){
+      players = new Player[2];
+      players[0] = player1;
+      players[1] = player2;
       board = new String[size][size];
       for(int i = 0;i<size;i++){
            for(int j = 0;j<size;j++){
@@ -25,14 +30,36 @@ public class Board{
       }        
    }
    
-   public void play(int position,Player player){
-      if (position>=0 && position <8){
-         for(int i = 0;i<size;i++){
-            if (board[i][position] == nonPlayedPosition ){
-               if (i == size-1){
-                  board[i][position] = player.representation;
+   private Boolean PlayerInGame(Player player){
+        for(Player pl:players){
+               if (pl == player){
+                  return true;
                }
-           }
+        }
+        return false;
+   }
+   
+   public void play(int position,Player player){
+      //System.out.println(PlayerInGame(player);
+      if (PlayerInGame(player)){
+         if (position>=0 && position <8){
+            for(int i = 0;i<size;i++){
+               if (board[i][position] == nonPlayedPosition ){
+                  if (i == size-1){
+                     board[i][position] = player.representation;
+                     break;
+                   }
+               }
+               if (board[i][position] != nonPlayedPosition){
+                     if(i>=1){
+                          board[i-1][position] = player.representation;
+                          break;
+                     }
+                  }
+            }
+         }
+         else{
+            System.out.println("No in players");
          }
       }
    }
--- connect4.java
+++ connect4.java
@@ -5,8 +5,11 @@ import connectFour.Player;
 public class connect4{
    public static void main(String[] args){
       Player player = new Player("S");
-      Board board = new Board();
+      Player player2 = new Player("J");
+      Board board = new Board(player,player2);
       board.play(2,player);
+      board.play(1,player2);
+      board.play(2,player3);
       board.print();
    }
 }
